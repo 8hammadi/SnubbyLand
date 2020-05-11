793feec082f88520d4589f5f313b77d7352e06a5
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-05-11 21:54:14 +0000

    ++++

--- CMakeFiles/SnubbyLand.dir/DependInfo.cmake
+++ CMakeFiles/SnubbyLand.dir/DependInfo.cmake
@@ -10,7 +10,8 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/include/SDL2"
+  "SDL2"
+  "SDL2/include"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/SnubbyLand.dir/flags.make
+++ build/CMakeFiles/SnubbyLand.dir/flags.make
@@ -6,5 +6,5 @@ CXX_FLAGS =
 
 CXX_DEFINES = 
 
-CXX_INCLUDES = -I/usr/include/SDL2/include 
+CXX_INCLUDES = -I/home/run/Desktop/SnubbyLand/SDL2 -I/home/run/Desktop/SnubbyLand/SDL2/include 
 
