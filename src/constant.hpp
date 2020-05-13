c35007fd014339d3b8abfdcf06d9ceb01bf6352d
Abdellatif-AitHammadi <abdellatif.aithammadi@gmail.com>
2020-05-13 23:57:33 +0000

    +++

---
+++ src/constant.hpp
@@ -0,0 +1,9 @@
+#define PI 3.141592653589793
+#define N_POPULATION 100
+#define NEURAL_NETWORK { (int)level.player.input.size(),40,10, 4}
+#define RANDOM (double)rand()/RAND_MAX
+#define TOKEN_SIZE 10
+// #define SERVER_STREAM "127.0.0.1:8000"
+#define SERVER_STREAM "snubbyland.herokuapp.com"
+#define SPEED 1
+#define COEURS 5
